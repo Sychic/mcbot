3b30bfa3c7b0f1cb0ddac438d071d9065972541b
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2021-02-03 23:39:12 -0500

    Dungeon Party bot

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 //imports
-require('dotenv').config({path:__dirname+'/.env'})
+const path = require("path");
+require('dotenv').config({
+    path: path.join(__dirname, '.env')
+});
 const mineflayer = require("mineflayer");
-const discord = require("discord.js");
+const Discord = require("discord.js");
 const ejs = require("emoji-js");
 require("colors");
+const wait = require("util").promisify(setTimeout);
 
 //variables
 const emojiConverter = new ejs();
 emojiConverter.text_mode = true;
 emojiConverter.colons_mode = true;
-let lastMsg = {"platform":null,"user":null,"cache":null};
+let lastMsg = {
+    "platform": null,
+    "user": null,
+    "cache": null
+};
 const date = new Date();
 let cache, cache2;
 let next = new Date();
 let passthrough = false;
 let officers = JSON.parse(process.env.OFFICERS);
 
-const client = new discord.Client({ ws : { intents : ['GUILDS','GUILD_MESSAGES']}});
+const client = new Discord.Client({
+    ws: {
+        intents: ['GUILDS', 'GUILD_MESSAGES']
+    }
+});
 const options = {
     host: 'mc.hypixel.net',
     version: '1.8.9',
@@ -27,7 +39,7 @@ const options = {
 // minecraft bot stuff vv
 let mc;
 (function init() {
-    try{
+    try {
         console.log("Logging in.");
         mc = mineflayer.createBot(options);
         mc._client.once("session", session => options.session = session);
@@ -38,22 +50,22 @@ let mc;
             }, 60000);
         });
 
-    }catch(e){
+    } catch (e) {
         console.error(e);
     }
 })();
 
 function callEveryHour() {
     mc.chat(`/gc Dark Auction Reminder~ (3m)`);
-    date.setHours(date.getHours()+1);
+    date.setHours(date.getHours() + 1);
     date.setMinutes(52);
     date.setSeconds(0);
     const difference = date - new Date();
-    next=new Date(Date.now()+difference);
-    setTimeout(function() {
+    next = new Date(Date.now() + difference);
+    setTimeout(function () {
         mc.chat(`/gc Jacob's Contest Reminder~ (3m)`);
     }, 20 * 60000); // 20 minutes to 3 minutes before Contest (Contest is always 20 after DA)
-    setTimeout(function(){
+    setTimeout(function () {
         callEveryHour();
     }, 60 * 60000); // 60 minutes
 }
@@ -70,40 +82,40 @@ if (date.getMinutes() === 52) {
     date.setSeconds(0);
 
     const difference = date - new Date();
-    next=new Date(Date.now()+difference);
+    next = new Date(Date.now() + difference);
     setTimeout(callEveryHour, difference);
 }
 
 let uuid;
 let name;
-mc.on("login", () => {
+mc.on("login", async () => {
     uuid = mc._client.session.selectedProfile.id;
     name = mc._client.session.selectedProfile.name;
-    setTimeout(() => {
-        console.log("Sending to limbo.");
-        mc.chat("/achat \u00a7c<3");
-    }, 1000);
+    await wait(1000);
+    console.log("Sending to limbo.");
+    mc.chat("/achat \u00a7c<3");
     // mc.chat("/gc Logged in")
 });
 
-mc.on("message", (chatMsg) => {
+let inParty = false;
+mc.on("message", async (chatMsg) => {
     const msg = chatMsg.toString();
     console.log("Minecraft: ".brightGreen + msg);
-    if(passthrough&&msg!==""){
+    if (passthrough && msg !== "") {
         return client.guilds.cache.get(process.env.GUILD).channels.cache.get(process.env.CHANNEL).send(msg);
     }
-    if(msg.startsWith("From")&&msg.includes(":")){
+    if (msg.startsWith("From") && msg.includes(":")) {
         client.guilds.cache.get(process.env.GUILD).channels.cache.get(process.env.PMS).send(msg);
         return;
     }
 
     if ((msg.startsWith("Guild >") || msg.startsWith("Officer >")) && msg.includes(":")) {
         let splitMsg = msg.split(" ");
-        if(splitMsg[2].includes(name)||splitMsg[3].includes(name)) return;
-        cache=splitMsg;
+        if (splitMsg[2].includes(name) || splitMsg[3].includes(name)) return;
+        cache = splitMsg;
         let i = msg.search(/:/);
-        let splitMsg2 = [msg.slice(0,i), msg.slice(i+1)];
-        cache2=splitMsg2;
+        let splitMsg2 = [msg.slice(0, i), msg.slice(i + 1)];
+        cache2 = splitMsg2;
         let sender, sentMsg;
         if (splitMsg[2].includes("[")) {
             sender = splitMsg[3];
@@ -112,33 +124,67 @@ mc.on("message", (chatMsg) => {
         }
         sentMsg = splitMsg2[1];
 
-        const embed = new discord.MessageEmbed()
+        const embed = new Discord.MessageEmbed()
             .setAuthor(sender + ": " + sentMsg, "https://minotar.net/helm/" + sender)
             .setColor("#AAAAAA");
-        const rank = splitMsg[2].replace(/[\[\]']+/g,'');
-        if(rank==="MVP++"){
+        const rank = splitMsg[2].replace(/[\[\]']+/g, '');
+        if (rank === "MVP++") {
             embed.setColor("#FFAA00")
-        } else if(rank==="MVP+"){
+        } else if (rank === "MVP+") {
             embed.setColor("#5555FF")
-        } else if(rank==="MVP"){
+        } else if (rank === "MVP") {
             embed.setColor("#55FFFF")
-        } else if(rank==="VIP+"){
+        } else if (rank === "VIP+") {
             embed.setColor("#00AA00")
-        } else if(rank==="VIP"){
+        } else if (rank === "VIP") {
             embed.setColor("#55FF55")
         }
         let c;
-        if (msg.startsWith("Guild >") && msg.includes(":") && process.env.gToggle==`true`) {
+        if (msg.startsWith("Guild >") && msg.includes(":") && process.env.gToggle == `true`) {
             c = client.guilds.cache.get(process.env.GUILD).channels.cache.get(process.env.CHANNEL);
-        } else if (msg.startsWith("Officer >") && msg.includes(":") && process.env.oToggle==`true`) {
+        } else if (msg.startsWith("Officer >") && msg.includes(":") && process.env.oToggle == `true`) {
             c = client.guilds.cache.get(process.env.OGUILD).channels.cache.get(process.env.OCHANNEL);
         }
         if (c === undefined) return;
         c.send(embed);
-        lastMsg={"user":sender,"platform":"minecraft","cache":embed};
+        lastMsg = {
+            "user": sender,
+            "platform": "minecraft",
+            "cache": embed
+        };
         return;
     }
-    mc.chat("/achat \u00a7ca");
+    if (msg.includes("has invited you to join their party!")) {
+        let usernameRegex = /(?:\[.+?\] )?(\w+)/;
+        if (usernameRegex.test(msg)) {
+            let username = usernameRegex.exec(msg)[1];
+            if (username) {
+                if (!inParty) {
+                    inParty = true;
+                    await wait(200);
+                    mc.chat(`/p join ${username}`);
+                    await wait(200);
+                    mc.chat(`/pc Hi ${username}! Please queue for your dungeon. I will leave the party in 15 seconds.`);
+                    await wait(15000);
+                    mc.chat("/p leave");
+                    inParty = false;
+                } else {
+                    await wait(200);
+                    mc.chat(`/msg ${username} Sorry! I'm currently helping someone else out! Please party me again in 15 seconds.`);
+                }
+            }
+        }
+    } else if (msg.includes("warped the party to a SkyBlock dungeon!")) {
+        await wait(500);
+        mc.chat("/pc Good luck!");
+        await wait(200);
+        mc.chat("/p leave");
+        inParty = false;
+    } else if (msg.includes("has disbanded the party!") || msg.includes("You are not in a party.")) {
+        inParty = false;
+    }
+    await wait(200);
+    mc.chat("/achat \u00a7c<3");
 });
 
 // discord bot stuff vv
@@ -147,7 +193,7 @@ client.on("ready", () => {
 });
 
 function processDiscordMsg(message) {
-    const name = (message.author.id === "342863217892261888") ? "Nahlee" :(message.author.id === "617450312625684523") ? "Isabel" :(message.guild.member(message.author).displayName.length>16) ? message.author.username : message.guild.member(message.author).displayName;
+    const name = (message.author.id === "342863217892261888") ? "Nahlee" : (message.author.id === "617450312625684523") ? "Isabel" : (message.guild.member(message.author).displayName.length > 16) ? message.author.username : message.guild.member(message.author).displayName;
     const emoteregex = /(:[^:\s]+:|<:[^:\s]+:[0-9]+>|<a:[^:\s]+:[0-9]+>)/g;
     let msg = emojiConverter.replace_unified(`(${name}): ` + message.cleanContent);
     let m = msg.match(emoteregex);
@@ -163,37 +209,41 @@ function processDiscordMsg(message) {
 }
 
 client.on("message", (message) => {
-    if((message.channel.id !== process.env.CHANNEL && message.channel.id !== process.env.OCHANNEL )|| message.author.bot) return;
-    if(officers.includes(message.author.id)&&message.content.startsWith(`${process.env.PREFIX}last`)){
+    if ((message.channel.id !== process.env.CHANNEL && message.channel.id !== process.env.OCHANNEL) || message.author.bot) return;
+    if (officers.includes(message.author.id) && message.content.startsWith(`${process.env.PREFIX}last`)) {
         message.channel.send(JSON.stringify(lastMsg));
         return;
-    }else if(officers.includes(message.author.id)&&message.content.startsWith(`${process.env.PREFIX}cache2`)){
+    } else if (officers.includes(message.author.id) && message.content.startsWith(`${process.env.PREFIX}cache2`)) {
         message.channel.send(JSON.stringify(cache2));
         return;
-    }else if(officers.includes(message.author.id)&&message.content.startsWith(`${process.env.PREFIX}cache`)){
+    } else if (officers.includes(message.author.id) && message.content.startsWith(`${process.env.PREFIX}cache`)) {
         message.channel.send(JSON.stringify(cache));
         return;
-    }else if(officers.includes(message.author.id)&&message.content.startsWith(`${process.env.PREFIX}next`)){
+    } else if (officers.includes(message.author.id) && message.content.startsWith(`${process.env.PREFIX}next`)) {
         message.channel.send(JSON.stringify(next));
         return;
-    }else if(officers.includes(message.author.id)&&message.content.startsWith(`${process.env.PREFIX}comm`)){
+    } else if (officers.includes(message.author.id) && message.content.startsWith(`${process.env.PREFIX}comm`)) {
         mc.chat(message.content.substring(6));
-        passthrough=true;
+        passthrough = true;
         setTimeout(() => {
-            passthrough=false;
+            passthrough = false;
         }, 250);
         return;
     }
-    if(message.content.startsWith(process.env.PREFIX)) return;
-    if(message.content === "") return;
+    if (message.content.startsWith(process.env.PREFIX)) return;
+    if (message.content === "") return;
     let processedMsg = processDiscordMsg(message);
     console.log("Discord: ".blue + message.author.username + ": " + message.content);
-    if (message.channel.id === process.env.CHANNEL){
+    if (message.channel.id === process.env.CHANNEL) {
         mc.chat("/gc " + processedMsg);
-    }else if (message.channel.id === process.env.OCHANNEL){
+    } else if (message.channel.id === process.env.OCHANNEL) {
         mc.chat("/oc " + processedMsg);
     }
-    lastMsg={"user":name,"platform":"discord",cache:"message.content"};
+    lastMsg = {
+        "user": name,
+        "platform": "discord",
+        cache: "message.content"
+    };
 });
 
 client.login(process.env.TOKEN).then();
